7faf54d7380b8e873acc4d69ab8607733eea57ec
Ricardo-Tzuc <ricardotz9911@gmail.com>
2024-04-02 11:35:12 -0600

    details and comments added to 09 Class

--- 09-ECMAScript6-modules-main.mjs
+++ 09-ECMAScript6-modules-main.mjs
@@ -1,3 +1,16 @@
+/*
+    For modular, it is enough that the file has the extension ".js",
+    but the V8 engine, which runs in the browsers, recommends using
+    the extension ".mjs".
+    -This is useful because it is clear which file is a module
+    and which is not.
+    -It ensures that module files are treated as such by
+    the Babel or Node.js extension.
+
+ *//*
+these functions can be considered secret or inaccessible cus
+the class use them, but we cannot access to them or modify
+from the import file. */
 function videoPlay(id) {
     const secretUrl = 'https://platziultrasecretomasquelanasa.com/' + id
     console.log("It's playing from "+ secretUrl);
@@ -7,7 +20,8 @@ function videoPause(id) {
     console.log("It's pausing from "+ secretUrl);
 }
 
-export class PlatziClass {
+// the export at the beginning of the declaration
+ export class PlatziClass {
     constructor({
         name,
         videoID,
@@ -16,12 +30,12 @@ export class PlatziClass {
         this.videoID = videoID
     }
 
-
     play(){
         videoPlay(this.videoID)
     }
-
     pause() {
         videoPause(this.videoID)
     }
 }
+// the export in only one declaration at the end of the module archive.
+// export {PlatziClass}
--- 09-ECMAScript6-modules-module.mjs
+++ 09-ECMAScript6-modules-module.mjs
@@ -1,13 +1,23 @@
+// The import is used in the script where we want to use the modules
 import { PlatziClass } from './09-ECMAScript6-modules-main.mjs'
+/* Rename is allowed in imports and exports,
+by using the keyword 'as' the new names is given:
+import/export {PlatziClass as ClassGenerator} from '/route'
+*/
 
+//we generate a new instance by using the imported prototype
 const class67 = new PlatziClass({
     name: 'JavaScript: OOP, based on prototypes',
     videoID: 'sadasdqweqdd8as8da9'
+    // by passing the ID will be helpful for the code hidden.
 })
 
 
 class67.play()
 class67.pause()
-
+// Using the methods seems easy, but the code-logic is in the main js.
 class67.play()
 class67.pause()
+
+// How to apply the module in HTML?
+//<script type="module" src="main.js"></script>
